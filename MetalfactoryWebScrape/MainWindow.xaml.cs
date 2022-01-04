32a736d209a437d9030928f4c1d848b930713800
Winter Thane <saso.basic.2012@gmail.com>
2022-01-04 21:03:08 +0100

    big rework, reduced requests to 1

--- MetalfactoryWebScrape/MainWindow.xaml.cs
+++ MetalfactoryWebScrape/MainWindow.xaml.cs
@@ -1,7 +1,6 @@
 ﻿using HtmlAgilityPack;
-using System;
 using System.Collections.Generic;
-using System.Threading.Tasks;
+using System.Linq;
 using System.Windows;
 
 namespace MetalfactoryWebScrape
@@ -15,9 +14,9 @@ namespace MetalfactoryWebScrape
 
         private void Make_File_Button_Click(object sender, RoutedEventArgs e)
         {
-            List<string> ConcertLinks = GetConcertLinks("https://metalfactory.ch/events");
-            Task<List<Concert>> Concerts = GetConcerts(ConcertLinks);
-            _ = MakeCalendarFile(Concerts);
+            List<Concert> ScrappedList = GetConcertLinks("https://metalfactory.ch/events");
+            List<Concert> ConcertsList = ScrappedList.GroupBy(x => new { x.Bands, x.Date }).Select(x => x.First()).ToList();
+            var xxx = "";
         }
 
         private HtmlDocument GetDocument(string url)
@@ -27,63 +26,41 @@ namespace MetalfactoryWebScrape
             return doc;
         }
 
-        private List<string> GetConcertLinks(string url)
-        {
-            var htmlInfo = new List<string>();
-            HtmlDocument doc = GetDocument(url);
-            HtmlNodeCollection linkNodes = doc.DocumentNode.SelectNodes("//h2/a");
-            var baseUri = new Uri(url);
-
-            foreach (HtmlNode node in linkNodes)
-            {
-                string href = node.Attributes["href"].Value;
-                htmlInfo.Add(new Uri(baseUri, href).AbsoluteUri);
-            }
-
-            return htmlInfo;
-        }
-
-        private async Task<List<Concert>> GetConcerts(List<string> detailURLs)
+        private List<Concert> GetConcertLinks(string url)
         {
             List<Concert> concerts = new List<Concert>();
             int counter = 1;
+            HtmlDocument doc = GetDocument(url);
+            HtmlNodeCollection linkNodes = doc.DocumentNode.SelectNodes("//div[contains(@class, \"ic-content\")]");
 
-            try
+            foreach (var detail in linkNodes)
             {
-                foreach (var link in detailURLs)
+                var concertBands = detail.FirstChild.InnerText.Replace("Konzerte", "").Trim();
+                var concertDate = "";
+                if (detail.ChildNodes[2].InnerText.Contains("-"))
                 {
-                    HtmlDocument doc = GetDocument(link);
-                    var concertBands = "//h1";
-                    var concertDate = "//span[contains(@class, \"ic-single-next\")]";
-                    var concertTime = "//span[contains(@class, \"ic-single-starttime\")]";
-                    var concertLocation = "//div[contains(@class, \"details ic-details\")]/p";
-                    var concert = new Concert
-                    {
-                        Bands = doc.DocumentNode.SelectSingleNode(concertBands).InnerHtml.Replace("&amp;", "&").Trim(),
-                        Date = doc.DocumentNode.SelectSingleNode(concertDate).InnerHtml.Replace("&nbsp;", " "),
-                        Time = doc.DocumentNode.SelectSingleNode(concertTime).InnerHtml,
-                        Location = doc.DocumentNode.SelectSingleNode(concertLocation).InnerHtml.Replace("&nbsp;", "").Trim().Remove(0, 35)
-                    };
-                    concerts.Add(concert);
-                    reportText.Text += counter.ToString() + ": " + concert.Bands + "\n";
-                    counter++;
-                    await Task.Delay(TimeSpan.FromSeconds(2));
+                    concertDate = detail.ChildNodes[2].InnerText.Split('-')[0].Replace("&nbsp;", " ").Trim();
                 }
-            }
-            catch (Exception ex)
-            {
-                reportText.Text = "Error in GetConcerts: " + ex.Message.ToString();
+                else
+                {
+                    concertDate = detail.ChildNodes[2].InnerText.Replace("&nbsp;", " ");
+                }               
+                var concertTime = concertDate.Substring(concertDate.Length - 6).Trim();
+                var concertLocation = detail.ChildNodes[3].InnerText.Trim();
+                var concert = new Concert
+                {
+                    
+                    Bands = concertBands.Replace("&amp;", "&"),
+                    Date = concertDate.Remove(concertDate.Length - 6).Trim(),
+                    Time = concertTime,
+                    Location = concertLocation
+                };
+                concerts.Add(concert);
+                reportText.Text += counter.ToString() + ": " + concert.Bands + " *** Date: " + concert.Date + " *** Time: " + concert.Time + " *** Location: " + concert.Location + "\n";
+                counter++;
             }
 
             return concerts;
-        }
-
-        private async Task MakeCalendarFile(Task<List<Concert>> concertList)
-        {
-            foreach (var item in await concertList)
-            {
-                
-            }
         }
     }
 }
